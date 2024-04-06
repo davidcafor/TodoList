757b7aa7fe558f701d36afbb1d693529c6923436
davidcafor <david.cardena@netexlearning.com>
2024-04-06 12:39:14 +0200

    Add darkMode & iPad support

--- TodoList/Views/AddView.swift
+++ TodoList/Views/AddView.swift
@@ -22,7 +22,7 @@ struct AddView: View {
                 TextField("Type Something here...", text: $textFieldText)
                     .padding(.horizontal)
                     .frame(height: 50)
-                    .background(Color(red: 239/255.0, green: 239/255.0, blue: 240/255.0))
+                    .background(Color(UIColor.secondarySystemBackground))
                 .cornerRadius(10)
                 
                 Button(action: saveButtonPressed, label: {
